1324802ffa8e2882c0b3f6e795b0624073006acb
PrzemooMac <midnight.project2016@gmail.com>
2018-01-14 00:16:07 +0100

    Added QnA image handling

--- MieciuWedkarz-src/Dialogs/BasicQnAMakerDialog.cs
+++ MieciuWedkarz-src/Dialogs/BasicQnAMakerDialog.cs
@@ -1,61 +1,66 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-
-using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
 using Microsoft.Bot.Connector;
+using QnAMakerDialog;
 
 namespace Microsoft.Bot.Sample.QnABot
 {
-    [Serializable]
-    public class RootDialog :  IDialog<object>
+  [Serializable]
+  public class RootDialog : IDialog<object>
+  {
+    public async Task StartAsync(IDialogContext context)
     {
-        public async Task StartAsync(IDialogContext context)
-        {
-            /* Wait until the first message is received from the conversation and call MessageReceviedAsync 
-            *  to process that message. */
-            context.Wait(this.MessageReceivedAsync);
-        }
-
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
-        {
-            /* When MessageReceivedAsync is called, it's passed an IAwaitable<IMessageActivity>. To get the message,
-            *  await the result. */
-            var message = await result;
-            
-            var qnaSubscriptionKey = Utils.GetAppSetting("QnASubscriptionKey");
-            var qnaKBId = Utils.GetAppSetting("QnAKnowledgebaseId");
-
-            // QnA Subscription Key and KnowledgeBase Id null verification
-            if (!string.IsNullOrEmpty(qnaSubscriptionKey) && !string.IsNullOrEmpty(qnaKBId))
-            {
-                await context.Forward(new BasicQnAMakerDialog(), AfterAnswerAsync, message, CancellationToken.None);
-            }
-            else
-            {
-                await context.PostAsync("Please set QnAKnowledgebaseId and QnASubscriptionKey in App Settings. Get them at https://qnamaker.ai.");
-            }
-            
-        }
-
-        private async Task AfterAnswerAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
-        {
-            // wait for the next user message
-            context.Wait(MessageReceivedAsync);
-        }
+      /* Wait until the first message is received from the conversation and call MessageReceviedAsync 
+      *  to process that message. */
+      context.Wait(MessageReceivedAsync);
     }
 
-    // For more information about this template visit http://aka.ms/azurebots-csharp-qnamaker
-    [Serializable]
-    public class BasicQnAMakerDialog : QnAMakerDialog
+    private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
     {
-        // Go to https://qnamaker.ai and feed data, train & publish your QnA Knowledgebase.        
-        // Parameters to QnAMakerService are:
-        // Required: subscriptionKey, knowledgebaseId, 
-        // Optional: defaultMessage, scoreThreshold[Range 0.0 – 1.0]
-        public BasicQnAMakerDialog() : base(new QnAMakerService(new QnAMakerAttribute(Utils.GetAppSetting("QnASubscriptionKey"), Utils.GetAppSetting("QnAKnowledgebaseId"), "No good match in FAQ.", 0.5)))
-        {}
+      var message = await result;
+      await context.Forward(new BasicQnAMakerDialog(), AfterAnswerAsync, message, CancellationToken.None);
     }
+
+    private async Task AfterAnswerAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+    {
+      // wait for the next user message
+      context.Wait(MessageReceivedAsync);
+    }
+  }
+
+  // For more information about this template visit http://aka.ms/azurebots-csharp-qnamaker
+  [Serializable]
+  [QnAMakerService("2271ce4c39c3491883941f69d1ecfc90", "73015880-cac0-400a-af2b-fea56e55e406")]
+  public class BasicQnAMakerDialog : QnAMakerDialog<IMessageActivity>
+  {
+    public override async Task DefaultMatchHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+    {
+      if (result.Answer.StartsWith("http"))
+      {
+        var activity = context.Activity as Activity;
+        if (activity == null)
+          return;
+
+        var reply = activity.CreateReply("");
+        reply.Attachments = new List<Attachment>
+        {
+          new Attachment
+          {
+            ContentUrl = result.Answer,
+            ContentType = "image/png",
+            Name = "Bender_Rodriguez.png"
+          }
+        };
+
+        await new ConnectorClient(new Uri(activity.ServiceUrl)).Conversations.ReplyToActivityAsync(reply);
+      }
+      else
+      {
+        await Task.FromResult(base.DefaultMatchHandler(context, originalQueryText, result));
+      }
+    }
+  }
 }
