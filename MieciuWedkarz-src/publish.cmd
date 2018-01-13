7257d1ae662cbf7ebf831348f84c53d9395a25f6
Dominik <dodocss@gmail.com>
2018-01-13 20:38:34 +0100

    Add start project from Azure

---
+++ MieciuWedkarz-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=mieciuwedkarz-Web-Deploy.pubxml -p:Password=rZctN7wbdJjJTv7ukvcPJg3ewo9HhAEyMmotmqlryKpfksjNbiLmq8FhTP4s
+
